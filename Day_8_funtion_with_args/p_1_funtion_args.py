802cd45cc6cd5fd53d412039aaed6d04434d9b66
Deepak Verma <deekgsbv@gmail.com>
2021-06-26 22:24:42 +0530

    day eight

---
+++ Day_8_funtion_with_args/p_1_funtion_args.py
@@ -0,0 +1,14 @@
+# # greet function with some lines
+# def greet():
+#     print("Hey, Appy")
+#     print("Good to see you, Appy")
+
+# greet()
+
+# new greet function with some parameter -> variable name
+# and argument -> what it contains
+def greet_with_name(name):
+    print(f"Hey, {name}")
+    print(f"Good to see you, {name}")
+
+greet_with_name("Appy")
---
+++ Day_8_funtion_with_args/p_2_more_than_one_args.py
@@ -0,0 +1,16 @@
+
+# #  parameter -> variable name
+# #  argument -> what it contains
+# def more_inputs(name, location):
+#     print(f"Hey I'm {name}")
+#     print(f"I live in {location}")
+
+# more_inputs("Appy", "Proxima Centurari")
+
+# use of keyword arguments in function
+def more_inputs(name, location):
+    print(f"Hey I'm {name}")
+    print(f"I live in {location}")
+
+more_inputs(location = "Proxima Centurari", name = "Appy")
+

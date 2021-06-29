e3c8f7a4e7a05ae148d7f5a2c83b6a859364fec9
Deepak Verma <deekgsbv@gmail.com>
2021-06-29 15:05:48 +0530

    day twice again

--- .vscode/p_7.py
+++ .vscode/p_7.py
@@ -1,42 +1,34 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: ")
-text = input("Type your message: ").lower()
-shift = int(input("Type the shift number: "))
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    #TODO-3: What happens if the user enters a number/symbol/space?
+    #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
+    #e.g. start_text = "meet me at 3"
+    #end_text = "•••• •• •• 3"
+    position = alphabet.index(char)
+    new_position = position + shift_amount
+    end_text += alphabet[new_position]
+    
+  print(f"Here's the {cipher_direction}d result: {end_text}")
 
-#TODO-1: Create a different function called 'decrypt' that takes the 'text'
-#  and 'shift' as inputs.
-  #TODO-2: Inside the 'decrypt' function, shift each letter
-  #  of the 'text' *backwards* in the alphabet by the shift amount 
-  # and print the decrypted text.  
-  #e.g. 
-  #cipher_text = "mjqqt"
-  #shift = 5
-  #plain_text = "hello"
-  #print output: "The decoded text is hello"
-def cipher(input_text, shift_amount, given_direction):
-    your_text = ""
-    len_apha = len(alphabet)
-    if shift_amount > len_apha:
-        shift_amount %= len_apha
-    if given_direction == "decode":
-        shift_amount *= -1
-    for i in range(len(input_text)):
-        fount_at_index = alphabet.index(input_text[i])
-        new_position = shift_amount + fount_at_index
-        if new_position > (len_apha - 1):
-            
-            your_text += alphabet[new_position % len_apha]
-        else:
-            your_text += alphabet[new_position]
-    print(f"Here's {direction}d result: {your_text}")        
+#TODO-1: Import and print the logo from art.py when the program starts.
+#from art import logo
+#print(logo)
+#TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
+#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
+#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
+#Hint: Try creating a while loop that continues to execute the program if the user types 'yes'. 
 
-#TODO-3: Check if the user wanted to encrypt or decrypt 
-# the message by checking the 'direction' variable.
-#  Then call the correct function based on that 'drection' variable.
-#  You should be able to test the code to encrypt *AND* decrypt a message.
-# if direction != "encode" or direction != "decode":
-#     print("Bad Entry")   
-# else:
-#     cipher(input_text=text,shift_amount=shift,given_direction=direction)
-cipher(input_text=text,shift_amount=shift,given_direction=direction)
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+#TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+#Try running the program and entering a shift number of 45.
+#Add some code so that the program continues to work even if the user enters a shift number greater than 26. 
+#Hint: Think about how you can use the modulus (%).
+shift %= 26
+caesar(start_text=text, shift_amount=shift, cipher_direction=direction)

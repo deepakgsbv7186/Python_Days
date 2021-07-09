f44b3ee5a36ff4385b6e23206575e4b4a5b6f776
Deepak Verma <deekgsbv@gmail.com>
2021-07-09 23:57:58 +0530

    day twelve

---
+++ Day_12_gobal_variable/p_2_number_guess.py
@@ -0,0 +1,34 @@
+# display game logo
+from art import logo
+print(logo)
+# generate a random number range (1,100)
+import random
+computer_choose = random.randrange(1,100)
+print(computer_choose)
+def compare_with(choose,level):
+    ''' take random generated number and difficulty level,
+        depending on level they get chances to guess '''
+    finish = False
+    if level == "h":
+        lives = 3
+    else:
+        lives = 5
+    while not finish:
+        print(f"You have {lives} chances left.")
+        lives -= 1
+        guess_by_user = int(input("Enter Number: "))
+        if lives <= 0:
+            finish = True
+            return "You Lose The Game"
+        elif guess_by_user == choose:
+            finish = True
+            return "You Win"
+        elif guess_by_user > choose:
+            print("High")
+        elif guess_by_user < choose:
+            print("Low")
+
+# take input for difficulty level
+diff_level = input("H -> Hard or L -> Low: ").lower()
+# now function call 
+print(compare_with(computer_choose,diff_level))

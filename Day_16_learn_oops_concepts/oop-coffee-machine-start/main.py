05b9ccbec6825e30bd92dc55aea87db2ea252627
Deepak Verma <deekgsbv@gmail.com>
2021-09-25 22:45:43 +0530

    day 16 coffee maker with oops

--- Day_16_learn_oops_concepts/oop-coffee-machine-start/main.py
+++ Day_16_learn_oops_concepts/oop-coffee-machine-start/main.py
@@ -1,31 +1,30 @@
-from menu import Menu, MenuItem
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-# menu = Menu()
-# menu_item = MenuItem()
 coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
-# take user input for coffee
-# print(menu.get_items())
-# choice = input("What would you like to order: ")
-# if choice == "report":
-#     money_machine.report()
-# elif choice == "off":
-#     pass
-# else:
-#     print(menu.find_drink(choice))
-coffee_maker.report()
-money_machine.report()
+menu = Menu()
 
-# check for the resources
+is_on = True
 
-# ask for the money required
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like to order {options}: ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        # check for the resources
+        if coffee_maker.is_resource_sufficient(drink):
+            # ask for the money required
+            # check for the successfull transaction
+            if money_machine.make_payment(drink.cost):
+                # now make coffee and update resources 
+                coffee_maker.make_coffee(drink)
+        else:
+            print("Sorry we run out of resources")
 
-# check for the successfull transaction
-
-# now make coffee
-
-# report keyword for check resources
-
-# off keyword for turn off coffee machine
